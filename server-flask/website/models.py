edfcde07b604d111dc15c3dabdd31f5c1397ab24
Melvin <melvingit@gmail.com>
2024-01-05 18:35:01 -0800

    user and solve models created

--- server-flask/website/models.py
+++ server-flask/website/models.py
@@ -1,6 +1,15 @@
 from . import db
 from flask_login import UserMixin
 from mongoengine import *
+import datetime
 
 class User(Document, UserMixin):
-    username = StringField()
+    username = StringField(required=True)
+    password = StringField(required=True)
+    date = DateTimeField(default=datetime.datetime.utcnow)
+
+class Solve(Document):
+    time = DecimalField(required=True)
+    user = ReferenceField(User)
+    plustwo = BooleanField()
+    
